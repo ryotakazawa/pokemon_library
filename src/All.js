330e15eccbbbd8918e273e0b8c8917ee2f9b7788
高沢崚 <ryotakazawaengineer@gmail.com>
2020-06-28 23:01:55 +0900

    総数を表示

--- src/All.js
+++ src/All.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function All() {
-  return(
+  const [data, setData] = useState({ results: [] });
+  const [url, setUrl] = useState(
+      'https://pokeapi.co/api/v2/pokemon?limit=151&offset=0',
+  );
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const result = await axios(url);
+
+        setData(result.data);
+      } catch (error) {
+        setIsError(true);
+      }
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [url]);
+
+  return (
     <>
-      <p>ハローワールド</p>
+      {isError && <div>Something went wrong ...</div>}
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+      <ul>
+        {data.count}
+      </ul>
+      )}
     </>
   );
 }
